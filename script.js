57d81453b9aa1f6be9f932c91b3817a62f9db622
Quang <quangnghia0412@gmail.com>
2026-08-17 17:53:04 -0600

    ui before the design, will based on this

--- script.js
+++ script.js
@@ -1,64 +1,59 @@
+//Album name, list and info//
 const songs = [
-    {number: 1, title: "Elegie", duration: "1:27"},
-    {number: 2, title: "IDK", duration: "3:16"},
-    {number: 3, title: "Wtf Bby I'm Lit", duration: "2:46"},
-    {number: 4, title: "Anh Không Muốn Nó Dễ Dàng", duration: "2:45"},
-    {number: 5, title: "Baby (feat. marzuz)", duration: "2:53"},
-    {number: 6, title: "Yêu Anh Giết Anh", duration: "2:45"},
-    {number: 7, title: "Mắt Môi Tay Chân (feat.Tage)", duration: "3:12"},
-    {number: 8, title: "Đao Của Anh Vừa", duration: "2:04"},
-    {number: 9, title: "Là Gì Của Nhau", duration: "2:22"},
-    {number: 10, title: "Night In Prague", duration: "3:33"},
-    {number: 11, title: "Một Cái Ôm", duration: "3:21"},
-    {number: 12, title: "Liệm", duration: "3:53"},
-    {number: 13, title: "Nếu Như Ta Chẳng Còn (feat.A$AP Ướt Mi)", duration: "5:17"},
-    {number: 14, title: "Ai Mới Là Kẻ Xấu Xa", duration: "3:11"},
-    {number: 15, title: "Slippery (feat.Tùng Dương)", duration: "3:35"},
-    {number: 16, title: "Intenpol", duration: "0:53"},
-    {number: 17, title: "Tây Thi", duration: "1:44"},
-    {number: 18, title: "Hút Và Hút", duration: "2:14"},
-    {number: 19, title: "Dưa Chua", duration: "3:02"},
-    {number: 20, title: "Xa Xôi (feat.Obito)", duration: "3:37"},
-    {number: 21, title: "Che Phủ", duration: "2:35"},
-    {number: 22, title: "Oanh M = Thuoc", duration: "3:24"},
-    {number: 23, title: "Ghet Xog Lai Thik", duration: "1:53"},
-    {number: 24, title: "Nhìn Kẻ Thù Của Tao", duration: "3:54"},
-    {number: 25, title: "Envy (feat.THANHDRAW)", duration: "3:55"},
-    {number: 26, title: "Cảm Ơn", duration: "2:39"},
-    {number: 27, title: "Không Cần Lo Cho Tao", duration: "2:36"},
-    {number: 28, title: "Huh (feat.RPT Orijinn & THANKDRAW)", duration: "4:11"},
-    {number: 29, title: "Nguyễn Văn Mười", duration: "4:02"},
-    {number: 30, title: "Thịt Lợn", duration: "3:48"},
+    {number: 2, title: "IDK", duration: "3:16", link:'../HVL/IDK.flac'},
+    {number: 3, title: "Wtf Bby I'm Lit", duration: "2:46", link:'../HVL/WTF.flac'},
+    {number: 4, title: "Anh Không Muốn Nó Dễ Dàng", duration: "2:45", link:'../HVL/AKMNDD.flac'},
+    {number: 5, title: "Baby (feat. marzuz)", duration: "2:53", link:'../HVL/Baby.flac'},
+    {number: 6, title: "Yêu Anh Giết Anh", duration: "2:45", link:'../HVL/YAGA.flac'},
+    {number: 7, title: "Mắt Môi Tay Chân (feat.Tage)", duration: "3:12", link:'../HVL/MMTC.flac'},
+    {number: 8, title: "Đao Của Anh Vừa", duration: "2:04", link:'../HVL/DCAV.flac'},
+    {number: 9, title: "Là Gì Của Nhau", duration: "2:22", link:'../HVL/LGCN.flac'},
+    {number: 10, title: "Night In Prague", duration: "3:33", link:'../HVL/NIP.flac'},
+    {number: 11, title: "Một Cái Ôm", duration: "3:21", link:'../HVL/MCO.flac'},
+    {number: 12, title: "Liệm", duration: "3:53", link:'../HVL/Liem.flac'},
+    {number: 13, title: "Nếu Như Ta Chẳng Còn (feat.A$AP Ướt Mi)", duration: "5:17", link:'../HVL/NNTCC.flac'},
+    {number: 14, title: "Ai Mới Là Kẻ Xấu Xa", duration: "3:11", link:'../HVL/AMLKXX.flac'},
+    {number: 15, title: "Slippery (feat.Tùng Dương)", duration: "3:35", link:'../HVL/Slippery.flac'},
+    {number: 16, title: "Intenpol", duration: "0:53", link:'../HVL/Intenpol.flac'},
+    {number: 17, title: "Tây Thi", duration: "1:44", link:'../HVL/TT.flac'},
+    {number: 18, title: "Hút Và Hút", duration: "2:14", link:'../HVL/HVH.flac'},
+    {number: 19, title: "Dưa Chua", duration: "3:02", link:'../HVL/DuaChua.flac'},
+    {number: 20, title: "Xa Xôi (feat.Obito)", duration: "3:37", link:'../HVL/XX.flac'},
+    {number: 21, title: "Che Phủ", duration: "2:35", link:'../HVL/ChePhu.flac'},
+    {number: 22, title: "Oanh M = Thuoc", duration: "3:24", link:'../HVL/OMBT.flac'},
+    {number: 23, title: "Ghet Xog Lai Thik", duration: "1:53", link:'../HVL/GXLT.flac'},
+    {number: 24, title: "Nhìn Kẻ Thù Của Tao", duration: "3:54", link:'../HVL/NKTCT.flac'},
+    {number: 25, title: "Envy (feat.THANHDRAW)", duration: "3:55", link:'../HVL/Envy.flac'},
+    {number: 26, title: "Cảm Ơn", duration: "2:39", link:'../HVL/CamOn.flac'},
+    {number: 27, title: "Không Cần Lo Cho Tao", duration: "2:36", link:'../HVL/KCLCT.flac'},
+    {number: 28, title: "Huh (feat.RPT Orijinn & THANKDRAW)", duration: "4:11", link:'../HVL/Huh.flac'},
+    {number: 29, title: "Nguyễn Văn Mười", duration: "4:02", link:'../HVL/NVM.flac'},
+    {number: 30, title: "Thịt Lợn", duration: "3:48", link:'../HVL/TL.flac'},
 ]
 
-function createSong(song) {
-    const track = document.createElement('div');
-    track.className = 'track';
-    track.dataset.trackNumber = song.number;
+//Fit songs in the web//
 
-    track.innerHTML = `
-                    <div class="left-side">
-                        <span class="track-number">${song.number}</span>
-                        <span class="play-button">▶</span>
-                    </div>
 
-                    <div class="song-info">
-                        <p class="track-name">${song.title}</p>
-                        <p class="artist">RPT MCK</p>
-                    </div>
 
-                    <button class="save">+</button>
-                    <span class="duration">${song.duration}</span>
-                    <button class="more-option">⋯</button>
-    `;
+//Play music//
+const selectTrack = document.getElementById('selectTrack');
+const selectButton = document.getElementById('play-button');
 
-    return track;
+const testTrack = document.getElementById('testTrack');
+const testButton = document.getElementById('testButton');
+
+function playMusic() {
+    selectTrack.addEventListener('click', function() {
+        testTrack.play();
+    });
 }
 
+function playButton() {
+    selectButton.addEventListener('click', function() {
+        event.stopPropagation();
+        testButton.play();
+    });
+}
 
-const musicList = document.querySelector('.music');
-
-songs.forEach (song => {
-    const track = createSong(song);
-    musicList.appendChild(track);
-});
+playMusic();
+playButton();
